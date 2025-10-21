349f0e861eee379a9cc2757dced1e8e97aa0be2f
heaminjo <157016835+heaminjo@users.noreply.github.com>
2025-10-21 19:19:16 +0900

    feature:swipper 빌드 시 문제 수정

---
+++ mimic-leaderboard/src/types/swiper.d.ts
@@ -0,0 +1,6 @@
+declare module 'swiper/vue';
+declare module 'swiper/css';
+declare module 'swiper/css/autoplay';
+declare module 'swiper/css/navigation';
+declare module 'swiper/css/pagination';
+declare module 'swiper/modules';
